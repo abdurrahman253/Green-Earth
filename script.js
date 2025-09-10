baea83c355d9feb6150c701854652c596cfe6625
Abdur Rahman <abdur@example.com>
2025-09-10 21:46:07 +0600

    updated index.html and added new features in script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const categoryContainer = document.getElementById("category_container");
 const plantsContainer = document.getElementById("plants_container")
 const modalContainer  = document.getElementById("modal_container")
 const plantsModal = document.getElementById("plants_modal");
+const cartContainer = document.getElementById("cart_container");
+const cartCount = document.getElementById("cart_count")
+
+let addToCart = []
 
 // load Category 
 const loadCategory = () => {
@@ -32,6 +36,7 @@ const showCategory = (categories) => {
 // Adding event listener with category container 
 categoryContainer.addEventListener("click", (event) => {
     if( event.target.localName === "li") {
+        showLoading();
         loadPlantsByCategory(event.target.id)
     }
 
@@ -101,9 +106,6 @@ const loadPlants = () => {
 
 // show all plants 
 const showPlants = (plants) => {
-   
-    
-      const eachId = plants[1] ;
     plants.forEach( (plant) => {
       
          plantsContainer.innerHTML += `
@@ -121,7 +123,10 @@ const showPlants = (plants) => {
                                 <div class="">৳ <span class="font-bold text-xl text-[#15803D]"> ${plant.price} </span> </div>
                           </div>
 
-                         <button class="text-lg font-bold rounded-full btn btn-accent">Add to Cart</button>
+                         <button  class="cursor-pointer flex justify-center items-center gap-2 px-6 py-3 rounded-full font-bold text-white bg-gradient-to-r from-green-500 to-green-700 shadow-md hover:from-green-600 hover:to-green-800 hover:scale-95 transition-all duration-300">
+                         <i class="fa-solid fa-cart-shopping"></i>
+                         Add to Cart
+                         </button>
                     </div>
                  </div>
        
@@ -141,7 +146,7 @@ plantsContainer.addEventListener("click", (event) => {
 
 })
 
-
+ 
 handleModal = (event) => {
     const id = event.target.parentNode.parentNode.id ;
    fetch(`https://openapi.programming-hero.com/api/plant/${id}`)
@@ -156,6 +161,8 @@ handleModal = (event) => {
 }
 
 
+
+// creating modal
 const showPlantsDetails = (plant) => {
     plantsModal.showModal()
 
@@ -182,6 +189,88 @@ const showPlantsDetails = (plant) => {
 }
 
 
+
+
+// Adding loading Spinner
+const showLoading = () => {
+    plantsContainer.innerHTML = `
+     <div class="flex justify-center items-center min-h-[200px] space-y-4"
+         
+        
+    </div>
+    `
+}
+
+
+// Adding Cart 
+plantsContainer.addEventListener("click" , (event) => {
+   if(event.target.innerText === 'Add to Cart'){
+    handleAddToCart(event) 
+   }
+})
+
+
+
+ 
+const handleAddToCart = (event) => {
+    const treeName = event.target.parentNode.children[0].innerText
+    const plantsPrice = parseInt(event.target.parentNode.children[2].children[1].children[0].innerText) ;
+     
+    const id = event.target.parentNode.parentNode.id ;
+    addToCart.push({
+       treeName : treeName,
+       price : plantsPrice,
+       id : id
+    })
+
+    showAddToCart(addToCart)
+   }
+ 
+   // showing add to cart 
+   const showAddToCart = (addToCart) => {
+    cartContainer.innerHTML = "";
+  
+    let total = 0;
+
+     addToCart.forEach(atc => {
+     total += atc.price;
+
+       cartContainer.innerHTML += `
+         <div class="flex items-center justify-between p-4 mb-3 transition bg-white border border-gray-200 shadow-sm rounded-xl hover:shadow-md">
+                  <div>
+                    <h1 class="text-lg font-semibold text-gray-800"> ${atc.treeName} </h1>
+                    <span class="text-green-600 font-bold text-sm">৳ ${atc.price} </span>
+                  </div>
+
+                  <button onclick="handleDeleteButton('${atc.id}') " class="px-3 py-1 text-sm font-medium text-red-500 border border-red-400 rounded-lg hover:bg-red-500 hover:text-white transition">
+                    Remove
+                  </button>
+       `
+     })
+      
+
+     if (addToCart.length > 0) {
+      cartContainer.innerHTML += `
+      <div class="flex justify-between items-center p-4 mt-4 bg-green-50 border border-green-200 rounded-xl shadow-sm">
+       <h2 class="text-lg font-bold text-gray-800">Total:</h2>
+       <span class="text-xl font-bold text-green-700">৳ ${total} </span>
+      </div>
+
+      `;
+     };
+     cartCount.innerText = addToCart.length;
+
+   } ;
+  
+
+
+   // handle delete button 
+   const handleDeleteButton = (addToCartId) => {
+     const filteredAtc = addToCart.filter(atc => atc.id !== addToCartId)
+     addToCart = filteredAtc 
+     showAddToCart(addToCart)
+    
+   }
 
 
  loadCategory();
